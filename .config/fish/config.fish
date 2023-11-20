3b214be214998564362d9cb38fbb853e95c19abe
yusa <141967852+yusamock@users.noreply.github.com>
2023-11-19 21:09:07 -0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,6 @@ set fish_greeting
 alias v='nvim'
 alias nfc='v .config/neofetch/config.conf'
 alias fc='v .config/fish/config.fish'
-alias nc='ncspot'
 alias cp='cp -r'
 alias mkdir='mkdir -p'
 alias fetch='clear && pfetch'
@@ -17,20 +16,6 @@ alias pr='sudo pacman -Rns'
 alias off='poweroff'
 alias rb='reboot'
 alias xrx='xrdb merge .Xresources'
-alias kp='killall polybar && polybar lmao & disown'
 
     # Commands to run in interactive sessions can go here
-end
-
-
-# Prompt function
-
-function fish_prompt
-	
-	set_color #7D8284
-	printf '%s' (prompt_pwd)
-
-	set_color normal
-	printf '∮ '
-
 end
